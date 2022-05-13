0cb64f257bae43da10c45de979e6a6c506ca130d
QuietSolitude <loneliersinner@gmail.com>
2022-05-14 01:59:55 +0800

    index

--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,10 @@
   <div class="page">
     <header class="header">
       <div class="title">
-        <div class="logo">
-          Logo
-        </div>
+        <Logo class="logo" />
 
         <div class="text">
-          DigiTAO Software
+          DigiTAO <br> Software
         </div>
       </div>
 
@@ -29,6 +27,11 @@ export default Vue.extend({})
 </script>
 
 <style scoped>
+@font-face {
+  font-family: "Raleway";
+  src: url("/fonts/Raleway.ttf") format("truetype");
+}
+
 .header {
   display: flex;
   justify-content: space-between;
@@ -36,5 +39,22 @@ export default Vue.extend({})
 
 .title {
   display: flex;
+}
+
+.logo {
+  width: 26px;
+  height: 34px;
+  stroke: #242424;
+  margin-right: 8px;
+  stroke-width: 8.25;
+  shape-rendering: crispEdges;
+}
+
+.text {
+  display: flex;
+  align-items: flex-end;
+  font-family: Raleway, sans-serif;
+  font-weight: bold;
+  font-size: 14px;
 }
 </style>
