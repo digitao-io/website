4c5e89ab4301bd4d477bc9698aba139673efcdfc
digitao-io <contact@digitao.io>
2022-05-14 17:45:20 +0200

    Implemented the menu button component.

---
+++ components/Menu/MenuButton.vue
@@ -0,0 +1,38 @@
+<template functional>
+  <button class="menu-button">
+    <svg
+      class="menu-button__icon"
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <path v-if="props.open" d="M0 3h24M0 21h24M0 12h24" />
+      <path v-else d="M1 23 23 1M1 1l22 22" />
+    </svg>
+  </button>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    open: { type: Boolean, required: true }
+  }
+})
+</script>
+
+<style scoped>
+.menu-button {
+  margin: 0;
+  border: none;
+  padding: 0;
+  background-color: transparent;
+}
+
+.menu-button__icon {
+  stroke: #242424;
+  stroke-width: 2;
+}
+</style>
