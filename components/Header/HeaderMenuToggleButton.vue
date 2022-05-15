8db2cdcac668d85f997c6a4630d3eb19b80531ec
digitao-io <contact@digitao.io>
2022-05-15 17:18:13 +0200

    Fixed a typo.

--- components/Header/HeaderMenuToggleButton.vue
+++ components/Header/HeaderMenuToggleButton.vue
@@ -1,7 +1,7 @@
 <template>
-  <button class="header-menu-toggle-button" @click.native="handleClick">
+  <button class="header-menu-toggle-button" @click="handleClick">
     <svg
-      class="header-menu-toggle-butto__icon"
+      class="header-menu-toggle-button__icon"
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
