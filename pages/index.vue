171a9c0d4928d4a6040f3d7095b9b10abb55101c
digitao-io <contact@digitao.io>
2022-05-27 22:03:37 +0200

    Added an example of fetch.

--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,26 @@
     <Header />
 
     <main class="content">
-      Content
+      Content: {{ foo }}
     </main>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data () {
+    return {
+      foo: ''
+    }
+  },
+
+  async fetch () {
+    this.foo = await Promise.resolve('hello world')
+  }
+})
+</script>
 
 <style scoped>
 .page {
