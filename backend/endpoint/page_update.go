8083d7dd915f6b520b11b808331e6bb18241f843
digitao-io <contact@digitao.io>
2024-07-17 09:28:34 +0200

    Implemented page_update.

--- backend/endpoint/page_update.go
+++ backend/endpoint/page_update.go
@@ -1,1 +1,53 @@
 package endpoint
+
+import (
+	"digitao.io/website/app"
+	"digitao.io/website/model"
+	"github.com/doug-martin/goqu/v9"
+	_ "github.com/doug-martin/goqu/v9/dialect/mysql"
+	"github.com/gin-gonic/gin"
+)
+
+func PageUpdate(ctx *app.Context) gin.HandlerFunc {
+	return func(g *gin.Context) {
+		param := model.PageIdentifier{}
+		err := g.ShouldBindQuery(&param)
+		if err != nil {
+			app.ResponseWithParseError(g, "Cannot parse request query parameters")
+			return
+		}
+
+		data := model.Page{}
+		err = g.ShouldBindJSON(&data)
+		if err != nil {
+			app.ResponseWithParseError(g, "Cannot parse request body")
+			return
+		}
+
+		query, args, err := ctx.SqlBuilder.
+			Update("pages").
+			Set(
+				goqu.Record{
+					"key":        data.Key,
+					"menu_name":  data.MenuName,
+					"content_id": data.ContentId,
+				},
+			).
+			Where(
+				goqu.C("key").Eq(param.Key),
+			).
+			ToSQL()
+		if err != nil {
+			app.ResponseWithUnknownError(g, err)
+			return
+		}
+
+		_, err = ctx.Database.Exec(query, args...)
+		if err != nil {
+			app.ResponseWithUnknownError(g, err)
+			return
+		}
+
+		app.ResponseWithOk(g)
+	}
+}
