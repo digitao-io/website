91b5c198adbbb36c4107477de0d7ca1f049be9be
QuietSolitude <loneliersinner@gmail.com>
2024-07-23 01:39:22 +0800

    markdown

--- frontend/resources/src/components/MarkdownComponent/MarkdownComponent.vue
+++ frontend/resources/src/components/MarkdownComponent/MarkdownComponent.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
-import { defineProps } from "vue";
+import { defineProps, ref } from "vue";
 import type { PropType } from "vue";
+import { Marked } from 'marked';
+import { markedHighlight } from "marked-highlight";
+import hljs from 'highlight.js';
+import 'highlight.js/styles/github.css'; 
 
 export type MarkdownComponentConfig = {
   markdown: string;
@@ -9,11 +13,23 @@ export type MarkdownComponentConfig = {
 const props = defineProps({
   config: { type: Object as PropType<MarkdownComponentConfig>, required: true },
 });
+
+const marked = new Marked(
+  markedHighlight({
+    langPrefix: 'hljs language-',
+    highlight(code, lang, info) {
+      const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+      return hljs.highlight(code, { language }).value;
+    }
+  })
+);
+
+
+const htmlContent = ref(marked.parse(props.config.markdown));
 </script>
 
 <template>
-  <div class="markdown-component">
-    {{ config.markdown }}
+  <div class="markdown-component" v-html="htmlContent">
   </div>
 </template>
 
