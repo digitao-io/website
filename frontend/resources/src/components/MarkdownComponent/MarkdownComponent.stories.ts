cdb3a08ace20e35ddf12da7a85e54be75e0fbb88
digitao-io <contact@digitao.io>
2024-07-23 03:27:28 +0200

    Updated linting rules, fixed some linting problems, improved the implementation of the MarkdownComponent.

--- frontend/resources/src/components/MarkdownComponent/MarkdownComponent.stories.ts
+++ frontend/resources/src/components/MarkdownComponent/MarkdownComponent.stories.ts
@@ -12,13 +12,13 @@ export default meta;
 type Story = StoryObj<typeof MarkdownComponent>;
 
 const markdown = `
-# This should be h2
+# This should be h1
 This is the first paragraph
 
-## This should be h3
+## This should be h2
 This is the second paragraph
 
-## Another headline
+## Another headline h2
 
 \`\`\`js
 // The syntax highlighting should work:
@@ -27,6 +27,8 @@ const a = 42;
 
 Some random text:
 * This is a list
+  * Sub-list point 1
+  * Sub-list point 2
 * Another point
 `;
 
--- frontend/resources/src/components/MarkdownComponent/MarkdownComponent.vue
+++ frontend/resources/src/components/MarkdownComponent/MarkdownComponent.vue
@@ -1,36 +1,43 @@
 <script setup lang="ts">
-import { defineProps, ref } from "vue";
+import { computed, defineProps } from "vue";
 import type { PropType } from "vue";
-import { Marked } from 'marked';
+
+import { Marked } from "marked";
 import { markedHighlight } from "marked-highlight";
-import hljs from 'highlight.js';
-import 'highlight.js/styles/github.css'; 
+import hljs from "highlight.js";
+import "highlight.js/styles/github.css";
 
 export type MarkdownComponentConfig = {
   markdown: string;
 };
 
-const props = defineProps({
-  config: { type: Object as PropType<MarkdownComponentConfig>, required: true },
-});
-
 const marked = new Marked(
   markedHighlight({
-    langPrefix: 'hljs language-',
-    highlight(code, lang, info) {
-      const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+    langPrefix: "hljs language-",
+    highlight: (code, lang) => {
+      const language = hljs.getLanguage(lang) ? lang : "plaintext";
       return hljs.highlight(code, { language }).value;
-    }
-  })
+    },
+  }),
 );
 
+const props = defineProps({
+  config: {
+    type: Object as PropType<MarkdownComponentConfig>,
+    required: true,
+  },
+});
 
-const htmlContent = ref(marked.parse(props.config.markdown));
+const htmlContent = computed(() => marked.parse(props.config.markdown));
 </script>
 
 <template>
-  <div class="markdown-component" v-html="htmlContent">
-  </div>
+  <!-- eslint-disable vue/no-v-html -->
+  <div
+    class="markdown-component"
+    v-html="htmlContent"
+  />
+  <!-- eslint-enable vue/no-v-html -->
 </template>
 
 <style scoped>
