277b31002afbf0877f60e49a05661a6c0f5b6e2c
digitao-io <contact@digitao.io>
2024-07-24 01:45:26 +0200

    Improved the implementation of the default template.

--- frontend/resources/src/templates/DefaultTemplate/DefaultTemplate.vue
+++ frontend/resources/src/templates/DefaultTemplate/DefaultTemplate.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+import { ref } from "vue";
+import DefaultTemplateMenuButton from "./DefaultTemplateMenuButton.vue";
+
 export type DefaultTemplateConfig = {
   primaryMenuEntries: DefaultTemplateConfigMenuEntry[];
   secondaryMenuEntries: DefaultTemplateConfigMenuEntry[];
@@ -12,34 +15,50 @@ export type DefaultTemplateConfigMenuEntry = {
 const props = defineProps<{
   config: DefaultTemplateConfig;
 }>();
+
+const menuActivated = ref(false);
+
+const toggleMenuActivated = () => {
+  menuActivated.value = !menuActivated.value;
+};
 </script>
 
 <template>
   <header class="page-header">
-    <div class="logo-area">
-      <div class="page-header__logo">
-        logo
-      </div>
+    <div class="logo">
+      <svg
+        class="logo-svg"
+        viewBox="0 0 32 42"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M21 21H31V41H1V11H21V21ZM21 21V31H11V21H21Z" />
+        <path d="M31 1H21V11H31V1Z" />
+      </svg>
 
-      <div class="page-header__logo-text">
-        digiTAO <br> Software
+      <div class="logo-text">
+        digiTAO<br>Software
       </div>
     </div>
 
     <div class="page-header__menu-container">
-      [加上菜单按钮与菜单等内容]
-      <nav class="page-header__main-menu">
-        <p>该部分内容仅作示例，请删除：</p>
-        <ul>
-          <li
-            v-for="menuEntry of props.config.primaryMenuEntries"
-            :key="menuEntry.url"
-          >
-            {{ menuEntry.label }}
-          </li>
-        </ul>
-      </nav>
+      <default-template-menu-button
+        :activated="menuActivated"
+        @click="toggleMenuActivated"
+      />
     </div>
+
+    <!--nav class="page-header__main-menu">
+      <p>该部分内容仅作示例，请删除：</p>
+      <ul>
+        <li
+          v-for="menuEntry of props.config.primaryMenuEntries"
+          :key="menuEntry.url"
+        >
+          {{ menuEntry.label }}
+        </li>
+      </ul>
+    </nav-->
   </header>
   <main class="page-content">
     <slot name="main" />
@@ -63,17 +82,21 @@ const props = defineProps<{
 <style scoped>
 .page-header {
   display: flex;
+  align-items: center;
   justify-content: space-between;
 }
-.logo-area {
+
+.logo {
   display: flex;
+  align-items: end;
 }
-.page-header__logo {
-  width: 50px;
-  height: 60px;
+.logo-svg {
+  stroke: var(--color-primary);
+  width: 32px;
+  stroke-width: 2;
 }
-.page-header__logo-text {
-  text-align: left;
+.logo-text {
+  margin-left: 10px;
   font-family: var(--font-raleway);
   font-size: 14px;
   font-weight: bold;
---
+++ frontend/resources/src/templates/DefaultTemplate/DefaultTemplateMenuButton.vue
@@ -0,0 +1,158 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const props = defineProps<{
+  activated: boolean;
+}>();
+
+const initialState = ref(true);
+</script>
+
+<template>
+  <button
+    class="menu-button"
+    :class="{
+      'menu-button-activated': props.activated && !initialState,
+      'menu-button-deactivated': !props.activated && !initialState,
+    }"
+    @click="initialState = false"
+  >
+    <div />
+  </button>
+</template>
+
+<style scoped>
+
+.menu-button {
+  display: block;
+  margin: 0 -1.2em;
+  border: 0;
+  background-color: transparent;
+  font-size: 7px;
+  cursor: pointer;
+  padding: 3em 1.5em;
+}
+
+.menu-button > div {
+  width: 4em;
+  height: 0.3em;
+  background-color: var(--color-primary);
+}
+
+.menu-button > div::before,
+.menu-button > div::after {
+  transition: font-size 0s;
+  content: "";
+  display: block;
+  position: absolute;
+  width: 4em;
+  height: 0.3em;
+  background-color: var(--color-primary);
+}
+.menu-button > div::before {
+  margin-top: -1.5em;
+}
+.menu-button > div::after {
+  margin-top: 1.5em;
+}
+
+.menu-button > div {
+  transition: 0.4s;
+}
+
+.menu-button > div::before,
+.menu-button > div::after {
+  transition: 0.4s;
+  transform-origin: center center;
+}
+
+.menu-button-activated > div::before {
+  animation: stripe-top-activated 0.4s linear forwards;
+}
+.menu-button-activated > div {
+  background-color: transparent;
+}
+.menu-button-activated > div::after {
+  animation: stripe-bottom-activated 0.4s linear forwards;
+}
+
+.menu-button-deactivated > div::before {
+  animation: stripe-top-deactivated 0.4s linear forwards;
+}
+.menu-button-deactivated > div {
+  background-color: var(--color-primary);
+}
+.menu-button-deactivated > div::after {
+  animation: stripe-bottom-deactivated 0.4s linear forwards;
+}
+
+@keyframes stripe-top-activated {
+  0% {
+  }
+  20% {
+    margin-top: 0em;
+    transform: rotate(0deg);
+  }
+  60% {
+    margin-top: 0em;
+    transform: rotate(55deg);
+  }
+  100% {
+    margin-top: 0em;
+    transform: rotate(45deg);
+  }
+}
+
+@keyframes stripe-bottom-activated {
+  0% {
+  }
+  20% {
+    margin-top: 0em;
+    transform: rotate(0deg);
+  }
+  60% {
+    margin-top: 0em;
+    transform: rotate(-55deg);
+  }
+  100% {
+    margin-top: 0em;
+    transform: rotate(-45deg);
+  }
+}
+
+@keyframes stripe-top-deactivated {
+  0% {
+    margin-top: 0em;
+    transform: rotate(45deg);
+  }
+  20% {
+    transform: rotate(0deg);
+  }
+  60% {
+    margin-top: 1.7em;
+    transform: rotate(0deg);
+  }
+  100% {
+    margin-top: 1.5em;
+    transform: rotate(0deg);
+  }
+}
+
+@keyframes stripe-bottom-deactivated {
+  0% {
+    margin-top: 0em;
+    transform: rotate(-45deg);
+  }
+  20% {
+    transform: rotate(0deg);
+  }
+  60% {
+    margin-top: -1.7em;
+    transform: rotate(0deg);
+  }
+  100% {
+    margin-top: -1.5em;
+    transform: rotate(0deg);
+  }
+}
+</style>
