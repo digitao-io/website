15f8be29e223b35d80ddd0bb200e1776b9900947
digitao-io <contact@digitao.io>
2024-07-23 16:46:30 +0200

    Fixed an unused import.

--- frontend/resources/src/templates/DefaultTemplate/DefaultTemplate.vue
+++ frontend/resources/src/templates/DefaultTemplate/DefaultTemplate.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import type { PropType } from "vue";
-
 export type DefaultTemplateConfig = {
   primaryMenuEntries: DefaultTemplateConfigMenuEntry[];
   secondaryMenuEntries: DefaultTemplateConfigMenuEntry[];
