3cf52c9cf70b2bb42833c2ba2040d84642818fbb
QuietSolitude <loneliersinner@gmail.com>
2024-07-30 22:38:23 +0800

    butotn

--- frontend/backstage/src/views/LoginView.vue
+++ frontend/backstage/src/views/LoginView.vue
@@ -1,3 +1,37 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const username = ref("");
+const password = ref("");
+</script>
 <template>
-  <p>This is login</p>
+  <div class="from-table">
+    <span>User</span>
+    <input
+      v-model="username"
+      class="username-input"
+      placeholder="username"
+    >
+    <span>Password</span>
+    <input
+      v-model="password"
+      class="password-input"
+      type="password"
+      placeholder="password"
+    >
+    <button>Login</button>
+  </div>
 </template>
+
+<style scoped>
+.from-table {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+
+.username-input,
+.password-input {
+  margin-bottom: 16px;
+}
+</style>
