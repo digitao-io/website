724a1515fc1b082f41fc6be7000149649a6a3333
QuietSolitude <loneliersinner@gmail.com>
2024-08-03 14:48:32 +0800

    make some page

--- frontend/backstage/src/App.vue
+++ frontend/backstage/src/App.vue
@@ -1,7 +1,39 @@
 <script setup lang="ts">
-import { RouterView } from "vue-router";
+import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
+  <div class="test">
+    <div class="sidebar">
+      <RouterLink
+        class="home-content"
+        to="/"
+      >
+        Home
+      </RouterLink>
+      <RouterLink to="/files">
+        Files
+      </RouterLink>
+    </div>
+  </div>
   <RouterView />
 </template>
+
+<style scoped>
+.sidebar {
+  display: flex;
+  flex-direction: column;
+  padding: 0;
+  position: fixed;
+  width: 150px;
+}
+
+.content {
+  position: fixed;
+  top: 52px;
+  right: 0;
+  bottom:0;
+  overflow: scroll;
+}
+
+</style>
--- frontend/backstage/src/router/index.ts
+++ frontend/backstage/src/router/index.ts
@@ -1,6 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import LoginView from "@/views/LoginView.vue";
+import FilesView from "@/views/FilesView.vue";
+import ArticlesView from "@/views/ArticlesView.vue";
+import PagesView from "@/views/PagesView.vue";
+import ResourcesView from "@/views/ResourcesView.vue";
+import TagsView from "@/views/TagsView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +19,31 @@ const router = createRouter({
       path: "/login",
       name: "login",
       component: LoginView,
+    },
+    {
+      path: "/files",
+      name: "files",
+      component: FilesView,
+    },
+    {
+      path: "/articles",
+      name: "article",
+      component: ArticlesView,
+    },
+    {
+      path: "/pages",
+      name: "page",
+      component: PagesView,
+    },
+    {
+      path: "/resources",
+      name: "resource",
+      component: ResourcesView,
+    },
+    {
+      path: "/tags",
+      name: "tag",
+      component: TagsView,
     },
   ],
 });
